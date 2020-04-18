56d3ac6cc75649e64e8c872fafa3f08d8a182c47
Maicon Souza <maiconsouzapa@gmail.com>
2020-04-18 00:45:08 -0300

    Metodos de validacao de senha e email

--- src/teste/Validacao.java
+++ src/teste/Validacao.java
@@ -1,23 +1,68 @@
 package teste;
 
-public class Teste {
+public class Validacao {
 
-	public boolean verificaLetrasENumeros(String senha) {
-		char array [] = senha.toCharArray(); // transformando a String em um array de char
-		boolean existeNumero = false;
-		boolean existeLetra = false;
+	/**
+	 * Verifica se a senha possui ao menos uma letra e um numero
+	 * @param senha
+	 * @return true, caso a senha tiver ao menos uma letra e um numero
+	 */
+	public static boolean letrasENumeros(String senha)
+	{
+		char array [] = senha.toCharArray(); // transforma a String em um array de char
+		boolean existeNumero = false, existeLetra = false;
 		
-		for (int i = 0; i < array.length; i++) {
-			// verifica se char é um numero:
-			if (Character.isDigit(array[i])) {
-				existeNumero = true;
-			}
-			// verifica se char NÃO é um numero
-			if (! Character.isDigit(array[i])) {
-				existeLetra = true;
-			}
+		for (int i = 0; i < array.length; i++)
+		{			
+			if (Character.isDigit(array[i])) existeNumero = true; // verifica se o char é um numero
+			if (! Character.isDigit(array[i])) existeLetra = true; // verifica se o char é uma letra
 		}
 		
 		return (existeNumero && existeLetra);
 	}
+	
+	
+	/**
+	 * Verifica se existe o nome de usuario na senha
+	 * @param senha
+	 * @param username
+	 * @return true, caso nao exista nome de usuario na senha
+	 */
+	public static boolean usernameNaSenha(String senha, String username) {
+		return !senha.contains(username);
+	}
+	
+	
+	/**
+	 * Verifica se a senha atende o requisito de tamanho minimo permitido
+	 * @param senha
+	 * @return true, caso a senha seja valida
+	 */
+	public static boolean tamanhoMinimo(String senha) {
+		return (senha.length() >= 8);
+	}
+	
+	
+	/**
+	 * Verifica se um email e valido
+	 * @param email
+	 * @return true, caso o email seja valido
+	 */
+	public static boolean validaEmail(String email) {
+		return email.contains("@");
+	}
+	
+	
+	/**
+	 * Conjunto de validacoes para a senha
+	 * @param senha
+	 * @param username
+	 * @return true, caso todas as validacoes sejam bem sucedidas
+	 */
+	public static boolean validaSenha(String senha, String username) {
+		return letrasENumeros(senha)
+				&& usernameNaSenha(senha, username)
+				&& tamanhoMinimo(senha);
+	}
+	
 }
