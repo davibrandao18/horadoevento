149934b29d7e81e7398d3033dd293de9fabb4c5d
maicon <maicon@asus>
2020-04-28 22:54:16 -0300

    teste de reutilização de componentes

--- WebContent/presentation.js
+++ WebContent/presentation.js
@@ -2,7 +2,12 @@
 localStorage.setItem('pessoa', 'pf')
 
 //acao de clique no botao da empresa:
-function alteraPessoa(tipo) {
-    localStorage.setItem('pessoa', tipo)
-    console.log(localStorage.getItem('pessoa'))
-}
+function alteraPessoa() {
+		let btnHelper = document.getElementById('btnHelper');
+		let txtHelper = document.getElementById('textoHelper');
+		let txtPrincipal = document.getElementById('textoPrincipal');
+		console.log(txtHelper.value);
+		
+		txtHelper.innerHTML="teste";
+		console.log(txtHelper.value);
+	}
