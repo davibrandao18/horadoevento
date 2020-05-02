3dd65e8aa06f6d2d8545bb0b05504b46f18a652f
maicon <maicon@asus>
2020-05-02 14:36:43 -0300

    correção de warnings

--- src/dao/EmpresaUsuarioDao.java
+++ src/dao/EmpresaUsuarioDao.java
@@ -1,12 +1,10 @@
 package dao;
 
-import java.io.File;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import model.EmpresaUsuario;
-import model.Usuario;
 import service.UsuarioService;
 import service.EmpresaService;
 
