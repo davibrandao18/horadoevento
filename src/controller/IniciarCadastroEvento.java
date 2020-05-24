670195275e288d87bae674aa517b175466fcfd31
Davi Fonseca <davi.brandao2013@gmail.com>
2020-05-24 19:20:15 -0300

    Servlet para listar tags

---
+++ src/controller/IniciarCadastroEvento.java
@@ -0,0 +1,52 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Tag;
+import service.TagService;
+
+/**
+ * Servlet implementation class IniciarCadastroEvento
+ */
+@WebServlet("/perfil/empresa/IniciarCadastroEvento.do")
+public class IniciarCadastroEvento extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IniciarCadastroEvento() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		TagService ts = new TagService();
+		ArrayList<Tag> tags = ts.carregar();
+		
+		request.setAttribute("lista", tags);
+		
+		request.getRequestDispatcher("../../evento/novo/index.jsp").forward(request, response);
+	}
+
+}
