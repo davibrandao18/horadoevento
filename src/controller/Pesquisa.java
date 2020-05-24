f6c30779fd58c12bf7ff2960638367864b051bb7
Maicon Souza <maiconsouzapa@gmail.com>
2020-05-24 17:40:29 -0300

    retornando usuarios, eventos e empresas relativas ao parametro

--- src/controller/Pesquisa.java
+++ src/controller/Pesquisa.java
@@ -1,14 +1,20 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import service.EmpresaService;
+import service.EventoService;
+import service.UsuarioService;
 
 /**
- * Servlet implementation class Pesquisa
+ * Processa listas de usuarios, empresas e eventos semelhantes ao digitados na pesquisa
  */
 @WebServlet("/pesquisa/Pesquisa.do")
 public class Pesquisa extends HttpServlet {
@@ -18,16 +24,26 @@ public class Pesquisa extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		UsuarioService us = new UsuarioService();
+		EmpresaService es = new EmpresaService();
+		EventoService evs = new EventoService();
 		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String username = request.getParameter("username");
+		
+		System.out.println(us.listar(username).toString());
+		
+		session.setAttribute("listaUsuario", us.listar(username));
+		session.setAttribute("listaEmpresa", es.listar(username));
+		session.setAttribute("listaEvento", evs.listar(username));
+		
+		request.getRequestDispatcher("index.jsp").forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		doGet(request, response);
 	}
-
 }
