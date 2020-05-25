b7e84a564237802d5762960605ab96ca510b8acc
Davi Fonseca <davi.brandao2013@gmail.com>
2020-05-25 19:58:31 -0300

    Servlet de evento

--- src/controller/EventoController.java
+++ src/controller/EventoController.java
@@ -1,11 +1,15 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.Evento;
+import service.EventoService;
 
 /**
  * Servlet implementation class EventoController
@@ -26,16 +30,37 @@ public class EventoController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		String acao = request.getParameter("acao");
+		int id = Integer.parseInt(request.getParameter("eventoId"));
+		
+		Evento evento = null;
+		EventoService es = null;
+		
+		switch(acao) {
+			case "visualizar":{
+				evento = new Evento();
+				es = new EventoService();
+				
+				evento = es.carregar(id);
+				if (evento == null) {
+					request.getRequestDispatcher("/horadoevento/perfil/empresa/").forward(request, response);
+				} else {
+					request.setAttribute("evento", evento);
+					request.getRequestDispatcher("/horadoevento/evento/").forward(request, response);
+				}
+				
+			}
+		}
 	}
 
 }
