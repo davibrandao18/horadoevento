250793dc568a4611cf22e1592d1a9a474d664d86
Maicon Souza <maiconsouzapa@gmail.com>
2020-06-06 12:59:53 -0300

    corrigido merge

--- src/command/CancelarInscricao.java
+++ src/command/CancelarInscricao.java
@@ -6,14 +6,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-<<<<<<< HEAD
 import model.Usuario;
-=======
->>>>>>> branch 'master' of https://github.com/davibrandao18/horadoevento
-import service.InscricaoService;
 
-<<<<<<< HEAD
-public class CancelarInscricao implements Command{
+import service.InscricaoService;
+import service.UsuarioService;
+
+public class CancelarInscricao implements Command {
    
     @Override
     public void executar(HttpServletRequest request, HttpServletResponse response)
@@ -29,31 +27,16 @@ public class CancelarInscricao implements Command{
                 is.excluir(cpf);
                 us.excluirTags(user);
                 us.excluir(user.getUserName(), cpf);
+                is.excluir(request.getParameter("id_i"));
+                String mensagem = "Inscrição cancelada";
+                
+                request.getSession().setAttribute("sessao_mensagem", mensagem);
                 response.sendRedirect("/horadoevento/inicio/");
             }
             else request.getRequestDispatcher("erro/credenciais/").forward(request, response);;
             
         } catch (Exception e) {
             response.sendRedirect("/horadoevento/erro/not_found/");
-=======
-public class CancelarInscricao {
-    public void executar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
-        InscricaoService is = new InscricaoService();
-        
-        try {
-            is.excluir(request.getParameter("id_i"));
-            String mensagem = "Inscrição cancelada";
-            
-            request.getSession().setAttribute("sessao_mensagem", mensagem);
-            response.sendRedirect("/horadoevento/home/member/");
-        } catch (Exception e) {
-            e.printStackTrace();
->>>>>>> branch 'master' of https://github.com/davibrandao18/horadoevento
         }
-<<<<<<< HEAD
-        
-=======
->>>>>>> branch 'master' of https://github.com/davibrandao18/horadoevento
     }
 }
